b5807c71eb4e6db6133a6f3951b10ff125d02dfb
stanleywbwong <swong394@gmail.com>
2021-02-15 15:34:39 -0500

    followed along with Tutorial 5

--- Assignments/Tutorial_5/doggochat/app.py
+++ Assignments/Tutorial_5/doggochat/app.py
@@ -1,20 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, request, redirect, url_for
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 from database import *
 
 import re
 
 app = Flask(__name__, template_folder="templates", static_url_path='/static')
+auth = HTTPBasicAuth()
 
-@app.route('/')
+users = {
+    "melba": generate_password_hash("melba"),
+    "rose": generate_password_hash("rose"),
+    "chucky": generate_password_hash("chucky") 
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+
+@app.route('/feed')
+@auth.login_required
 def feed():
     posts = get_all_posts()
     handles = set([post["Handle"] for post in posts])
     return render_template('feed.html', 
                             posts=posts,
-                            handles=handles)
-    
+                            handles=handles,
+                            user=auth.current_user())
+
 @app.route('/dog/<string:handle>')
+@auth.login_required
 def dog(handle):
     dog = get_dog_by_handle(handle)
     posts = get_posts_by_handle(handle)
@@ -23,6 +41,33 @@ def dog(handle):
                             dog=dog,
                             posts=posts,
                             handles=handles)
+
+@app.route('/create', methods=['POST'])
+@auth.login_required
+def create():
+    post_content = request.form['post-content']
+    current_time = datetime.strftime(datetime.now(), '%m/%d/%Y, %H:%M:%S')
+    insert_post(auth.current_user(), post_content, current_time)
+    return redirect(url_for('feed'))
+
+@app.route('/delete')
+@auth.login_required
+def delete():
+    post_id = request.args.get('post_id')
+    delete_post(post_id, auth.current_user())
+    return redirect(url_for('feed'))
+
+@app.route('/')
+@auth.login_required(optional=True)
+def splash():
+    if auth.current_user():
+        return redirect(url_for('feed'))
+    else:
+        return render_template('splash.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('splash.html'), 401
 
 # Register filters to use rather than passing functions into render_template function
 
