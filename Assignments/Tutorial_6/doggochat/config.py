a33950be27a64d2ba81f9a9aacf2e834bf1e3578
stanleywbwong <33231705+stanleywbwong@users.noreply.github.com>
2021-03-05 11:39:52 -0500

    Delete config.py

--- Assignments/Tutorial_6/doggochat/config.py
+++
@@ -1,9 +0,0 @@
-server = "tcp:dogchat-stan.database.windows.net"
-database = "dogchat"
-username = "stanwong"
-password = "Swong394!"
-
-blob_container = "dogavatars"
-blob_cnxn = "DefaultEndpointsProtocol=https;AccountName=doggochatstorage;AccountKey=nN34r2dkhQOaCiFLsctauCsyQ3/bqz9jH6TetS0oxFAxoJm50dgMQHUwbPJJA9ui9tbqqxaZjbmPHZFksTTu8Q==;EndpointSuffix=core.windows.net"
-
-sendgrid_api_key = "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"
